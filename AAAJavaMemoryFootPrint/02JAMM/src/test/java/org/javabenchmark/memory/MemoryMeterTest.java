a42ed110359ca9423fb631bcac74dd9def3e127a
franck-benault <franck-benault@orange.fr>
2017-07-23 20:23:01 +0200

    memory footprint comparaison array list linkedlist

--- AAAJavaMemoryFootPrint/02JAMM/src/test/java/org/javabenchmark/memory/MemoryMeterTest.java
+++ AAAJavaMemoryFootPrint/02JAMM/src/test/java/org/javabenchmark/memory/MemoryMeterTest.java
@@ -1,6 +1,7 @@
 package org.javabenchmark.memory;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import org.github.jamm.MemoryMeter;
 import org.junit.Test;
@@ -12,18 +13,41 @@ public class MemoryMeterTest {
     @SuppressWarnings("restriction")
 	@Test
     public void shouldMeasureMemoryUsage() {
- 
+    	System.out.println("Array list");
         String st1 = "This is the string #1";
-        measure(st1);
+        //measure(st1);
          
         String st2 = "This is the string #2 and it has more chars.";
-        measure(st2);
+        //measure(st2);
          
         List<String> aList = new ArrayList<String>(0);
-        measure(aList);
+        //measure(aList);
          
         aList.add(st1);
+        //measure(aList);
+         
+        aList.add(st2);
         measure(aList);
+         
+        System.out.println(
+        		jdk.nashorn.internal.ir.debug.ObjectSizeCalculator.getObjectSize(aList));
+    }
+    
+    @SuppressWarnings("restriction")
+	@Test
+    public void shouldMeasureMemoryUsageLinkedList() {
+    	System.out.println("linked list");
+        String st1 = "This is the string #1";
+        //measure(st1);
+         
+        String st2 = "This is the string #2 and it has more chars.";
+        //measure(st2);
+         
+        List<String> aList = new  LinkedList<String>();
+        //measure(aList);
+         
+        aList.add(st1);
+        //measure(aList);
          
         aList.add(st2);
         measure(aList);
